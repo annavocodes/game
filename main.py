b55f3296b53cb8740d832d2a49d6e6a28c9d9619
Anna Vo <annavo1231@gmail.com>
2023-11-14 21:20:22 +0000

    LCD pattern

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import random
+from rpi_ws281x import PixelStrip, Color
+import time
+
+COUNT = 64
+PIN   = 12
+FREQ  = 800000
+DMA   = 10
+BRIGHTNESS = 50
+INVERT = False
+CHANNEL = 0
+
+strip = PixelStrip(COUNT, PIN, FREQ, DMA, BRIGHTNESS, INVERT, CHANNEL)
+strip.begin()
+
+def color(strip, color):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
 
 name = input("What is your name? ")
 print("Welcome " + name + "! I'm thinking of a number between 1 and 100")
@@ -27,13 +45,16 @@ for guess_number in range(1,11):
     print("Your guess is a little high. Try again.")
   else:
     break
+  color(strip, Color(255, 0, 0))
 
 if user_guess == my_number:
-  print("You won, " + name + "! You guessed my number in " +   
-str(guess_number) + " guesses.")
-  print("Your guesses were: " + " ".join(str(x) for x in guess))
+  color(strip, Color(0, 255, 0))
+  print("You won, " + name + "! You guessed my number in " + str(guess_number) + " guesses.")
+  print("Your guesses were: " + " ".join(str(x) for i in guess))
+  time.sleep(4)
+  color(strip, Color(0,0,0))
 else:
     print("Sorry! You didn't guess my number. The number I am thinking of is " + str(my_number))
-    
-# test commit
+    time.sleep(4)
+    color(strip, Color(0,0,0))
   
